c5e0474649243e06fb127217811e861190584415
Kiryl Yermakou <k.yermakou@a1.by>
2023-09-20 16:42:56 +0300

    dev: add first task

--- workspaces/react/src/App.tsx
+++ workspaces/react/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TextInput } from './components/TextInput/TextInput'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+    <pre style={{whiteSpace: 'pre-wrap', textAlign: 'left', display: 'block'}}>
+{`Task 1:
+  Write the code for <TextInput/> element,
+  which would highlight any text, 
+  surrounded by `} <span style={{background: 'green', color: 'white'}}>%%double-percent%%</span> {`;
+  
+  The input behavior should remain the same for user.
+`}
+    </pre>
+    <TextInput type='text' value={'make this input be able to highlight this %%text%%'} style={{width: 400}}/>
+  </>
 }
 
 export default App
